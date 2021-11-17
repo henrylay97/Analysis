0737e6a1d6c83673e217441d33c66b12fba6290d
Henry Lay <h.lay@lancaster.ac.uk>
2021-11-17 07:33:37 -0600

    First pass at NC PiZero selection

---
+++ find_me_an_analysis/NCPiZeroCuts.h
@@ -0,0 +1,53 @@
+const SpillCut kHasAtLeastOneShw([](const caf::SRSpillProxy *sp) {
+    if(sp->nslc == 0)
+      return false;
+
+    auto const &slc = sp->slc[kBestSliceID(sp)];
+    
+    return slc.reco.nshw > 0;  
+  });
+
+const SpillCut kLargestShwdEdx([](const caf::SRSpillProxy *sp) {
+    if(!kHasAtLeastOneShw(sp))
+      return false;
+
+    auto const &slc = sp->slc[kBestSliceID(sp)];
+    auto const &shw = slc.reco.shw[kLargestShwID(sp)];
+
+    return shw.bestplane_dEdx > 2;
+  });
+
+const SpillCut kLongestTrkLength([](const caf::SRSpillProxy *sp) {
+    if(sp->nslc == 0)
+      return false;
+
+    auto const &slc = sp->slc[kBestSliceID(sp)];
+    if(slc.reco.ntrk == 0) 
+      return true;
+
+    auto const &trk = slc.reco.trk[kLongestTrkID(sp)];
+
+    return trk.len < 50;
+  });
+
+
+const SpillCut kOtherBack = !kNCPiZero && !kCCNuMu && !kCCNuE && !kNC && !kPileUp;
+
+std::vector<TrueDef> ncpizero_sig_back_categories = {
+  {"Signal (NC #pi^{0})", kNCPiZero, kViolet+2, "Signal"},
+  {"CC #nu_{#mu}", kCCNuMu, kMagenta+2, "CCNuMu"},
+  {"CC #nu_{e}", kCCNuE, kCyan+2, "CCNuE"},
+  {"Other NC", kNC && !kNCPiZero, kOrange+2, "NC"}, 
+  {"PileUp", kPileUp, kTeal+8, "PileUp"},
+  {"Other", kOtherBack, kBlack, "Other"},
+  {"Background", !kNCPiZero, kBlack, "Background"},
+};
+
+std::vector<CutInfo> ncpizero_cuts = {
+  {"No Selection", kNoSpillCut, "NoCuts"},
+  {"Has #nu Slice", kHasNuSlc, "HasNuSlc"},
+  {"Reco FV", kRecoFV, "RecoFV"},
+  {"Has >=1 Showers", kHasAtLeastOneShw, "HasAtLeastOneShw"},
+  {"Largest Shower dEdx >2 MeV/cm", kLargestShwdEdx, "LargestShwdEdx"},
+  {"Longest Track Length < 50cm", kLongestTrkLength, "LongestTrkLength"},
+};
---
+++ find_me_an_analysis/basic_ncpizero_selection.C
@@ -0,0 +1,37 @@
+#include "sbnana/CAFAna/Core/Spectrum.h"
+#include "sbnana/CAFAna/Core/SpectrumLoader.h"
+#include "sbnana/CAFAna/StandardRecord/Proxy/SRProxy.h"
+#include "sbnana/SBNAna/Cuts/VolumeDefinitions.h"
+
+using namespace ana;
+
+#include "Structs.h"
+#include "TrueEventCategories.h"
+#include "TrueNuVars.h"
+#include "GenericCuts.h"
+#include "NCPiZeroCuts.h"
+
+#include "plot_a_selection.C"
+
+void basic_ncpizero_selection()
+{
+  setlocale(LC_NUMERIC, "");
+  const std::string inputName = "defname: test100_official_test100_MC2021Bv1_prodoverlay_corsika_cosmics_proton_genie_nu_spill_gsimple-configh-v1_tpc_reco2_caf_sbnd";
+
+  const double gPOT = 1e20;
+
+  std::vector cumulative_cuts = make_cumulative_cuts(ncpizero_cuts);
+  std::vector n_minus_one_cuts = make_n_minus_one_cuts(ncpizero_cuts);
+
+  std::vector all_cuts = ncpizero_cuts;
+  all_cuts.insert(all_cuts.end(), cumulative_cuts.begin(), cumulative_cuts.end());
+  all_cuts.insert(all_cuts.end(), n_minus_one_cuts.begin(), n_minus_one_cuts.end());
+
+  fill_and_save_spectra(inputName, all_cuts, ncpizero_sig_back_categories);
+
+  plot_selection(ncpizero_cuts, ncpizero_sig_back_categories, gPOT);
+  plot_selection(cumulative_cuts, ncpizero_sig_back_categories, gPOT);
+  plot_selection(n_minus_one_cuts, ncpizero_sig_back_categories, gPOT);
+
+  plot_cumulative_selection_metrics(cumulative_cuts, gPOT);
+}
